fecd201cf2f0aebd6c27d80e5999ee90adeca47f
mafia-creater <78353770+mafia-creater@users.noreply.github.com>
2025-01-13 17:13:25 +0530

    working on captain

--- backend/routes/captain.routes.js
+++ backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {body} = require('express-validator');
 const captainController = require('../controllers/captain.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 router.post('/register', [
     body('email').isEmail().withMessage('Please fill a valid email address'),
@@ -13,5 +14,13 @@ router.post('/register', [
     body('vehicle.vehicleType').isIn(['car', 'motorcycle', 'auto']).withMessage('Vehicle type must be car, motorcycle or auto')
 ], captainController.registerCaptain);
 
+router.post('/login', [
+    body('email').isEmail().withMessage('Please fill a valid email address'),
+    body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters')
+], captainController.loginCaptain);
+
+router.get('/profile', authMiddleware.authCaptain,captainController.getCaptainProfile);
+
+router.get('/logout', authMiddleware.authCaptain, captainController.logoutCaptain);
 
 module.exports = router;
